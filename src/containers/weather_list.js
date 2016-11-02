3444f3f50e8a4ca2b8df6a5485625c9cc13f99af
Georgia Mills <georgee_123@hotmail.com>
2016-11-02 14:04:17 +0000

    charts rendering inside application

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,7 +1,26 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Sparklines, SparklinesLine } from 'react-sparklines';
 
 class WeatherList extends Component {
+
+  renderWeather(cityData) {
+    const name = cityData.city.name
+    //pull off an array of temperatures
+    const temps = cityData.list.map(weather => weather.main.temp) //this will return the temp of each section in the list of temps for that city
+
+    return(
+      <tr key={name}>
+        <td> {name}</td>
+        <td>
+          <Sparklines height={120} width={180} data={temps}>
+            <SparklinesLine color="red" />
+          </Sparklines>
+        </td>
+      </tr>
+    )
+  }
+
   render() {
     return (
       <table className='table table-hover'>
@@ -14,7 +33,7 @@ class WeatherList extends Component {
           </tr>
         </thead>
         <tbody>
-
+          {this.props.weather.map(this.renderWeather)}
         </tbody>
       </table>
 
@@ -26,4 +45,4 @@ function mapStateToProps({ weather }) {
   return { weather } // ES6 syntax { weather } == {weather: weather}
 }
 
-export default conntct(mapStateToProps)(WeatherList);
+export default connect(mapStateToProps)(WeatherList);
