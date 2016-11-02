2379c813bfad9fa806a38c4a794b05c5dd0094e0
Georgia Mills <georgee_123@hotmail.com>
2016-11-02 13:46:25 +0000

    weather list container

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -5,7 +5,7 @@ export default function (state = [], action) {
     case FETCH_WEATHER:
 //      return state.concat([ action.payload.data ]);
       return [action.payload.data, ...state]; //ES6 way to write the above code
-      //this creates an entire new array to prevent state mutation 
+      //this creates an entire new array to prevent state mutation
   }
   return state;
 }
