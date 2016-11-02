146e05727359419eb8fad49e6031da908aa8770f
Georgia Mills <georgee_123@hotmail.com>
2016-11-02 12:28:41 +0000

    added weather reducer

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -3,13 +3,14 @@ import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { fetchWeather } from '../actions/index';
 
-export default class SearchBar extends Component {
+class SearchBar extends Component {
   constructor(props){
     super(props)
 
     this.state = { term: '' }
     //bind the contex of onInputChange
     this.onInputChange = this.onInputChange.bind(this)
+    this.onFormSubmit = this.onFormSubmit.bind(this)
   }
 
   onInputChange(event) {
@@ -17,6 +18,9 @@ export default class SearchBar extends Component {
   }
   onFormSubmit(event){
     event.preventDefault()
+
+    this.props.fetchWeather(this.state.term);
+    this.setState({term : '' }) //this sets the state to empty string, so the search bar empties
   }
   render() {
     return (
@@ -36,3 +40,9 @@ export default class SearchBar extends Component {
     );
   }
 }
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({ fetchWeather }, dispatch);
+}
+
+export default connect(null, mapDispatchToProps)(SearchBar)
